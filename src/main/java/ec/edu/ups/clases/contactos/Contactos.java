34f71655b81b236a2fe9d9d8ffe6189dd0fdb399
Edisson Diaz <edisonmarcelo3435@gmail.com>
2023-07-17 09:39:16 -0500

    first commit

---
+++ src/main/java/ec/edu/ups/clases/contactos/Contactos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.ups.clases.contactos;
+
+/**
+ *
+ * @author HP
+ */
+public class Contactos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
